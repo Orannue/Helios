65408bf258edb0b5dc8752543502cdb10e12d01e
Lan Chen <chenlaneva@mails.cuc.edu.cn>
2026-05-23 18:27:56 +0800

    add batch

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
-pip install -r requirements.txt
+PIP_CACHE_DIR="${PIP_CACHE_DIR:-$~/workplace/code/chenlan/pip_cache}"
+TRITON_CACHE_DIR="${TRITON_CACHE_DIR:-$/workplace/code/chenlan/triton/cache}"
 
-rm -rf ~/.triton/cache/
-rm -rf /tmp/torchinductor_*
+clear_compile_cache() {
+  rm -rf "$TRITON_CACHE_DIR"
+  if [ -n "${TORCHINDUCTOR_CACHE_DIR:-}" ]; then
+    rm -rf "$TORCHINDUCTOR_CACHE_DIR"
+  else
+    rm -rf /tmp/torchinductor_*
+  fi
+}
+
+pip install --cache-dir "$PIP_CACHE_DIR" -r requirements.txt
+
+clear_compile_cache
 
 pip uninstall triton torchao xformers wandb tensorflow tensorflow-cpu -y
-pip install wandb==0.23.0 triton==3.6.0
+pip install --cache-dir "$PIP_CACHE_DIR" wandb==0.23.0 triton==3.6.0
 
-rm -rf ~/.triton/cache/
-rm -rf /tmp/torchinductor_*
+clear_compile_cache
